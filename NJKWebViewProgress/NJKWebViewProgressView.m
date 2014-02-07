244bbac9b8a05b9f7e0734d4229bfb9fb98890af
Xu Zhe <xu.zhe@rakutec.com>
2014-02-07 23:32:29 +0900

    Fix a bug that will cause the progressView cannot get tintColor & Init the progressBar at zero width

--- NJKWebViewProgress/NJKWebViewProgressView.m
+++ NJKWebViewProgress/NJKWebViewProgressView.m
@@ -15,15 +15,20 @@
     if (self) {
         self.userInteractionEnabled = NO;
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-        _progressBarView = [[UIView alloc] initWithFrame:self.bounds];
+        
+        CGRect frame = self.bounds;
+        frame.size.width = 0.0; // should start from 0.0
+        _progressBarView = [[UIView alloc] initWithFrame:frame];
         _progressBarView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         UIColor *tintColor = [UIColor colorWithRed:22.f / 255.f green:126.f / 255.f blue:251.f / 255.f alpha:1.0]; // iOS7 Safari bar color
-        if ([UIApplication.sharedApplication.delegate.window respondsToSelector:@selector(setTintColor:)]) {
+        if ([UIApplication.sharedApplication.delegate.window respondsToSelector:@selector(setTintColor:)] &&
+            UIApplication.sharedApplication.delegate.window.tintColor) {    // Have to make sure there is tintColor!!!
             tintColor = UIApplication.sharedApplication.delegate.window.tintColor;
         }
         _progressBarView.backgroundColor = tintColor;
         [self addSubview:_progressBarView];
-
+        _progressBarView.alpha = 0.0;
+        
         _barAnimationDuration = 0.27f;
         _fadeAnimationDuration = 0.27f;
         _fadeOutDelay = 0.1f;
@@ -41,16 +46,16 @@
     BOOL isGrowing = progress > 0.0;
     [UIView animateWithDuration:(isGrowing && animated) ? _barAnimationDuration : 0.0 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
         CGRect frame = _progressBarView.frame;
-        frame.size.width = progress * self.bounds.size.width;
+        frame.size.width = MAX(progress, 0.0) * self.bounds.size.width;
         _progressBarView.frame = frame;
     } completion:nil];
-
-    if (progress >= 1.0) {
+    
+    if (progress < 0.0 || progress >= 1.0) {
         [UIView animateWithDuration:animated ? _fadeAnimationDuration : 0.0 delay:_fadeOutDelay options:UIViewAnimationOptionCurveEaseInOut animations:^{
             _progressBarView.alpha = 0.0;
         } completion:^(BOOL completed){
             CGRect frame = _progressBarView.frame;
-            frame.size.width = 0;
+            frame.size.width = 0.0;
             _progressBarView.frame = frame;
         }];
     }
